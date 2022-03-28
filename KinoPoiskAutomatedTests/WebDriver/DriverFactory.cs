88018513f3704e5ab8d57bcd51ed5e54085353ab
dasha_ivasha <dashaandolathebest@gmail.com>
2022-03-28 12:14:54 +0300

    *web driver

---
+++ KinoPoiskAutomatedTests/WebDriver/DriverFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace KinoPoiskAutomatedTests.WebDriver
+{
+    public class DriverFactory
+    {
+        public static IWebDriver GetDriver()
+        {
+            IWebDriver webDriver;
+            webDriver = new ChromeDriver();
+
+            return webDriver;
+        }
+    }  
+}
---
+++ KinoPoiskAutomatedTests/WebDriver/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+
+namespace KinoPoiskAutomatedTests.WebDriver
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private static IWebDriver _webDriver;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+
+        public IWebDriver CreateDriver()
+        {
+            if (_webDriver == null)
+            {
+                _webDriver = DriverFactory.GetDriver();
+            }
+
+            return _webDriver = DriverFactory.GetDriver();
+        }
+    }
+}
