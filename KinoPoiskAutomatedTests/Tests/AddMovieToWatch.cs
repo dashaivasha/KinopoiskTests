5e19632be83a36a1e598ee0b8d2057848ea15501
dasha_ivasha <dashaandolathebest@gmail.com>
2022-03-31 19:05:44 +0300

    final tests

---
+++ KinoPoiskAutomatedTests/Tests/AddMovieToWatch.cs
@@ -0,0 +1,20 @@
+﻿using KinoPoiskAutomatedTests.BaseClass;
+using NUnit.Framework;
+
+namespace KinoPoiskAutomatedTests.Tests
+{
+    [TestFixture]
+    public class AddMovieToWatchAndDelete : BaseTest
+    {
+        [Test]
+        public void AddFilmToWarchLaterAndDelete()
+        {
+            homePage.EnterFilmName(Data.FilmName);
+            homePage.SelectFilm();
+            filmPage.AddMovieToFavorites();
+            filmPage.GotoUserFolders();
+            myKinopoisk.DeleteFilm();
+            Assert.IsFalse(myKinopoisk.IsFilmAddded());
+        }
+    }
+}
